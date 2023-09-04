7cb1652b4a7fd84c6f159a620c77c01c218e019c
Emmanuel Giron <emmus.giron@gmail.com>
2023-09-03 23:47:05 -0600

    Captura de variables

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,14 +2,90 @@ import React, { useState } from "react";
 import Nav from "./Nav";
 import Footer from "./Footer";
 import Select from "./Select";
+import userEvent from "@testing-library/user-event";
 
 function Formulario(){
   const [destinyIsVisible,setDestinyIsVisible] = useState(false);
-  const [selectedOption, setSelectedOption] = useState(null);
+  const [selectedDestino, setSelectedDestino] = useState("panama");
+  const [pasajeros,setPasajeros] =useState(1);
+  const[costoUnidad,setCostoUnidad]=useState(0);
+  const[impuesto,setImpuesto]=useState(0);
+  const[total,setTotal]=useState(0);
+
+
+
 
   const handlePersonChange = (event) =>{
     setDestinyIsVisible(true)
+    setPasajeros(event.target.value)
   }
+  const handleDestinoChange = (newDestino) => {
+    setSelectedDestino(newDestino);}
+ 
+  const handleCostoUnitorio = () =>{
+    if(pasajeros === 1 && selectedDestino==="panama")
+    {
+        setCostoUnidad(197)
+    }else if(pasajeros === 1 && selectedDestino==="cancun")
+    {
+        setCostoUnidad(485)
+    }else if(pasajeros === 1 && selectedDestino==="chile")
+    {
+        setCostoUnidad(198)
+    }else if(pasajeros === 1 && selectedDestino==="peru")
+    {
+        setCostoUnidad(754)
+    }else if(pasajeros === 1 && selectedDestino==="honduras")
+    {
+        setCostoUnidad(565)
+    }else if(pasajeros === 2 && selectedDestino==="panama")
+    {
+        setCostoUnidad(165)
+    }else if(pasajeros === 2 && selectedDestino==="cancun")
+    {
+        setCostoUnidad(439)
+    }else if(pasajeros === 2 && selectedDestino==="chile")
+    {
+        setCostoUnidad(178)
+    }else if(pasajeros === 2 && selectedDestino==="peru")
+    {
+        setCostoUnidad(699)
+    }else if(pasajeros === 2 && selectedDestino==="honduras")
+    {
+        setCostoUnidad(499)
+    }else if((pasajeros === 3 || pasajeros === 4) && selectedDestino==="panama")
+    {
+        setCostoUnidad(149)
+    }else if((pasajeros === 3 || pasajeros === 4) && selectedDestino==="cancun")
+    {
+        setCostoUnidad(400)
+    }else if((pasajeros === 3 || pasajeros === 4) && selectedDestino==="chile")
+    {
+        setCostoUnidad(158)
+    }else if((pasajeros === 3 || pasajeros === 4) && selectedDestino==="peru")
+    {
+        setCostoUnidad(649)
+    }else if((pasajeros === 3 || pasajeros === 4) && selectedDestino==="honduras")
+    {
+        setCostoUnidad(459)
+    }else if(pasajeros === 5 && selectedDestino==="panama")
+    {
+        setCostoUnidad(129)
+    }else if(pasajeros === 5 && selectedDestino==="cancun")
+    {
+        setCostoUnidad(380)
+    }else if(pasajeros === 5 && selectedDestino==="chile")
+    {
+        setCostoUnidad(138)
+    }else if(pasajeros === 5 && selectedDestino==="peru")
+    {
+        setCostoUnidad(629)
+    }else if(pasajeros >= 5 && selectedDestino==="honduras")
+    {
+        setCostoUnidad(449)
+    }
+  }
+  console.log(costoUnidad)
     return (
         <div>
             <Nav/>
@@ -23,7 +99,7 @@ function Formulario(){
                 <form>
                   <fieldset className="mb-5 border border-black rounded-lg">
                       <div className="mb-5">
-                        <legend className="font-bold uppercase ml-2">Número de personas</legend>
+                        <legend className="font-bold uppercase ml-2">Número de Pasajeros</legend>
                       </div>
                   <div className="mb-5">
                       <input type="number" id="cantidad" name="cantidad" min="1" max="10" className="ml-5 mt-2 mb-2 border border-black rounded-lg text-center" 
@@ -36,7 +112,13 @@ function Formulario(){
                       <div className="mb-5">
                             <legend className="font-bold uppercase ml-2">Selección de destino</legend>
                       </div>
-                      <Select/>
+                      <Select
+                        selectedDestino={selectedDestino}
+                        onDestinoChange={(newDestino) => {
+                          handleDestinoChange(newDestino);
+                          handleCostoUnitorio();
+                        }}
+                      />
                     </fieldset>
                     )
                     }
--- src/components/Select.js
+++ src/components/Select.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { useState } from "react";
 
-
-function Select(){
- 
-
+function Select({ selectedDestino, onDestinoChange }) {
+    const [destino, setDestino] = useState(selectedDestino || "panama");
+    const handleDestino = (event) =>{
+        const newDestino = event.target.value;
+        setDestino(newDestino);
+        if (onDestinoChange) {
+        onDestinoChange(newDestino);
+        }
+    }
+    console.log(destino)
     return(
         <div className="mb-5 p-3">
-        <select name="select" className="rounded-lg w-full border border-black ">
+        <select name="select" className="rounded-lg w-full border border-black" onChange={handleDestino} value={destino}>
             <option value="panama">Ciudad de Pánama</option>
             <option value="cancun">Cancún México</option>
             <option value="chile">Santiago de chile</option>
@@ -14,7 +21,7 @@ function Select(){
             <option value="honduras">Roatán Honduras</option>
         </select>  
         </div>
-      
+        
     );
 }
 export default Select;
